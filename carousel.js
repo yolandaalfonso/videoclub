da9b4aaf2602ce8aecb017732e689d6b6441f8af
migue angel <plusangel07@gmail.com>
2025-06-24 11:22:41 +0200

    feat: Actualización Carrusel

--- carousel.js
+++ carousel.js
@@ -1,8 +1,18 @@
 function scrollCarousel(direction) {
   const carousel = document.getElementById("carousel");
-  const movieWidth = carousel.querySelector(".movie").offsetWidth + 20; // ancho + gap
-  carousel.scrollBy({
-    left: direction * movieWidth * 2, // puedes cambiar el 2 por cuántas películas desplazar por click
-    behavior: "smooth"
+//   //const movieWidth = carousel.querySelector(".movie").offsetWidth + 20; // ancho + gap
+//   carousel.scrollBy({
+//     left: direction * movieWidth * 2, // puedes cambiar el 2 por cuántas películas desplazar por click
+//     behavior: "smooth"
+//   });
+// }
+
+if (carousel) {
+  const scrollAmount = carousel.clientWidth;
+
+  carousel.scrollBy ( {
+    left: direction * scrollAmount,
+    behavior: "smooth",
   });
+}
 }
