90519fa9a2f96c68b27d660f2f620d8d3bab2261
Lin <lincarbajales@gmail.com>
2025-06-26 11:45:55 +0200

    Layout enlazado con CSS retocado

--- layout.js
+++ layout.js
@@ -1,28 +1,30 @@
 function crearLayout() {
-  
+
     const footer = document.getElementById('footer');
 
     footer.innerHTML = `
-            <div class="footer">
-                <h4>Videoclub</h4>
-                <p><a href="/politica-de-privacidad">Política de Privacidad</a></p>
-                <p><a href="/contacto">Contacto</a></p>
-            </div>
-            <div class="footer">
-                <h4>Cine</h4>
-                <p><a href="/politica-de-privacidad">Reserva</a></p>
-                <p><a href="/contacto">Cartelera</a></p>
-            </div>
-            <div class="footer">
-                <h4>Nosotros</h4>
-                <p><a href="/politica-de-privacidad">Conócenos</a></p>
-                <p><a href="/contacto">Empleo</a></p>
-            </div>
-            <div class="footer">
-                <h4>Contacto</h4>
-                <p><a href="/politica-de-privacidad">Contacto</a></p>
-                <p><a href="/contacto">Preguntas frecuentas</a></p>
-            </div>
+              <div class="footer-case">
+        <div class="footer">
+          <h4>Videoclub</h4>
+          <p><a href="/politica-de-privacidad">Política de Privacidad</a></p>
+          <p><a href="/contacto">Contacto</a></p>
+        </div>
+        <div class="footer">
+          <h4>Cine</h4>
+          <p><a href="/politica-de-privacidad">Reserva</a></p>
+          <p><a href="/contacto">Cartelera</a></p>
+        </div>
+        <div class="footer">
+          <h4>Nosotros</h4>
+          <p><a href="/politica-de-privacidad">Conócenos</a></p>
+          <p><a href="/contacto">Empleo</a></p>
+        </div>
+        <div class="footer">
+          <h4>Contacto</h4>
+          <p><a href="/politica-de-privacidad">Contacto</a></p>
+          <p><a href="/contacto">Preguntas frecuentas</a></p>
+        </div>
+      </div>
 `;
 }
 
